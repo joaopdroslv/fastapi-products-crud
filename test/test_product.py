5e4a8e4ca47915d10a2a37eefca992bd29709fbf
João Pedro <pdrojoao.slv@gmail.com>
2024-12-24 18:24:05 -0300

    new test created, tests title and summary improved

--- test/test_product.py
+++ test/test_product.py
@@ -61,15 +61,15 @@ def setup_database():
     mongo_client.drop_database(MONGODB_TEST_DATABASE_NAME)
 
 
-def test_create_product(setup_database):
-    """Ensuring the product will be created."""
+def test_create_product_successfully(setup_database):
+    """Checks if the product is created correctly."""
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post('/products', json=generated_products[0])
     assert response.status_code == 201
 
 
-def test_create_product_with_content_validation(setup_database):
-    """Ensuring the product will be created."""
+def test_create_product_validates_content_on_creation(setup_database):
+    """Checks whether the data sent when creating the product is validated correctly."""
     generated_products: List[dict] = utils.generate_valid_products(1)
     generated_product = generated_products[0]
     response = client.post('/products', json=generated_product)
@@ -83,7 +83,10 @@ def test_create_product_with_content_validation(setup_database):
 
 
 def test_create_invalid_in_stock_product(setup_database):
-    """Ensuring the product will not be created with invalid data."""
+    """
+    Checks whether the creation of the product with status "in_stock" 
+    and stock_quantity equal to 0 returns an error.
+    """
     generated_products: List[dict] = utils.generate_valid_products(1)
     invalid_product = generated_products[0].copy()
     # Quando o 'status' é 'em_estoque' é esperado o 'stock_quantity' seja > 0
@@ -93,8 +96,11 @@ def test_create_invalid_in_stock_product(setup_database):
     assert response.status_code == 422
 
 
-def test_create_invalid_out_of_stock_product(setup_database):
-    """Ensuring the product will not be created with invalid data."""
+def test_create_product_with_invalid_out_of_stock_status(setup_database):
+    """
+    Checks whether the creation of a product with status "out of stock" 
+    and stock_quantity greater than 0 returns an error.
+    """
     generated_products: List[dict] = utils.generate_valid_products(1)
     invalid_product = generated_products[0].copy()
     # Quando o 'status' é 'em_falta' é esperado o 'stock_quantity' seja == 0
@@ -104,8 +110,8 @@ def test_create_invalid_out_of_stock_product(setup_database):
     assert response.status_code == 422
 
 
-def test_create_product_with_invalid_price(setup_database):
-    """Ensuring the product will not be created with invalid data."""
+def test_create_product_with_invalid_price_value(setup_database):
+    """Checks whether creating a product with an invalid price returns an error."""
     generated_products: List[dict] = utils.generate_valid_products(1)
     generated_product = generated_products[0]
     generated_product['price'] = 0  # Seta um preço inválido, preço deve ser > 0
@@ -113,8 +119,8 @@ def test_create_product_with_invalid_price(setup_database):
     assert response.status_code == 422
 
 
-def test_list_products(setup_database):
-    """Ensuring all products will be listed."""
+def test_list_products_successfully(setup_database):
+    """Check if all products are listed correctly."""
     generated_products: List[dict] = utils.generate_valid_products(10)
     for generated_product in generated_products:
         client.post('/products', json=generated_product)
@@ -123,8 +129,8 @@ def test_list_products(setup_database):
     assert len(response.json()) == len(generated_products)
 
 
-def test_list_products_with_content_validation(setup_database):
-    """Ensuring all products will be listed."""
+def test_list_products_validates_content(setup_database):
+    """Checks that all products are listed and the data for each product is correct."""
     generated_products: List[dict] = utils.generate_valid_products(10)
     for generated_product in generated_products:
         client.post('/products', json=generated_product)
@@ -140,8 +146,8 @@ def test_list_products_with_content_validation(setup_database):
         assert resonse_data['stock_quantity'] == generated_product['stock_quantity']
 
 
-def test_list_product(setup_database):
-    """Ensuring the product will be listed."""
+def test_view_single_product(setup_database):
+    """Checks whether a single product can be displayed correctly."""
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post('/products', json=generated_products[0])
     created_product_id = response.json()['id']
@@ -151,8 +157,33 @@ def test_list_product(setup_database):
     assert response.json() == created_product
 
 
-def test_get_product_view_log(setup_database):
-    """Ensuring the product view log is being created."""
+def test_create_view_log_for_each_product_on_listing(setup_database):
+    """Checks if a viewing log is created for each listed product."""
+    generated_products: List[dict] = utils.generate_valid_products(10)
+    created_products = []
+    for generated_product in generated_products:
+        response = client.post('/products', json=generated_product)
+        created_products.append(response.json())  # Salva cada um dos produtos criados
+
+    # Lista os produtos 10x
+    for i in range (10):
+        client.get('/products')
+
+    # Para cada produto, verificar se existem 10 logs de visualização
+    for created_product in created_products:
+        product_id = created_product['id']
+        # Busca os logs de visualização do produto
+        response = client.get(f'/products/{product_id}/views')
+        response_data = response.json()
+        assert response.status_code == 200
+        assert 'number_of_views' in response_data
+        assert 'views' in response_data
+        assert response_data['number_of_views'] == 10  # Cada produto foi visualizado 10 vezes
+        assert len(response_data['views']) == 10  # Cada produto deve ter 10 logs
+
+
+def test_create_view_log_when_viewing_single_product(setup_database):
+    """Checks if the view log is created when viewing a single product."""
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post('/products', json=generated_products[0])
@@ -172,8 +203,8 @@ def test_get_product_view_log(setup_database):
     assert len(response_data['views']) == 1  # Produto foi pesquisado apenas uma vez
 
 
-def test_get_multiple_product_view_logs(setup_database):
-    """Ensuring the product view logs are being created."""
+def test_create_multiple_view_logs_for_single_product(setup_database):
+    """Checks if multiple view logs are created for a product viewed multiple times."""
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post('/products', json=generated_products[0])
@@ -194,8 +225,8 @@ def test_get_multiple_product_view_logs(setup_database):
     assert len(response_data['views']) == 30
 
 
-def test_product_view_log_format(setup_database):
-    """Ensuring the product view log is being created correctly."""
+def test_format_of_product_view_log(setup_database):
+    """Checks that product view logs are in the correct format."""
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post('/products', json=generated_products[0])
@@ -228,15 +259,18 @@ def test_product_view_log_format(setup_database):
         assert False
 
 
-def test_get_product_view_logs_of_a_nonexistent_product(setup_database):
-    """Ensuring NotFound exception when trying to get views logs a non-existing product."""
+def test_get_view_logs_of_nonexistent_product(setup_database):
+    """
+    Checks whether a NotFound error is returned when trying to 
+    access the view logs for a non-existent product.
+    """
     response = client.get(f'/products/999/views')
     assert response.status_code == 404
     assert response.json()['message'] == 'Product not found.'
 
 
-def test_update_product(setup_database):
-    """Ensuring the product will be updated."""
+def test_update_product_successfully(setup_database):
+    """Checks whether the product can be updated correctly."""
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post(f'/products', json=generated_products[0])
@@ -260,8 +294,8 @@ def test_update_product(setup_database):
     assert response_data['stock_quantity'] == updated_product['stock_quantity']
 
 
-def test_update_product_with_fewer_fields(setup_database):
-    """Ensuring the product will be updated with fewer fields."""
+def test_update_product_with_partial_data(setup_database):
+    """Checks if the product can be updated with partial data."""
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post(f'/products', json=generated_products[0])
@@ -275,16 +309,19 @@ def test_update_product_with_fewer_fields(setup_database):
     assert response.json()['stock_quantity'] == 999
 
 
-def test_update_a_nonexistent_product(setup_database):
-    """Ensuring NotFound exception when trying to update a non-existing product."""
+def test_update_nonexistent_product(setup_database):
+    """Checks if a NotFound error is returned when trying to update a non-existent product."""
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.put('/products/999', json=generated_products[0])
     assert response.status_code == 404
     assert response.json()['message'] == 'Product not found.'
 
 
-def test_update_invalid_in_stock_product(setup_database):
-    """Ensuring the product will not be updated with invalid data."""
+def test_update_product_with_invalid_in_stock_status(setup_database):
+    """
+    Checks whether updating a product with status "in_stock" 
+    and stock_quantity equal to 0 returns an error.
+    """
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post(f'/products', json=generated_products[0])
@@ -298,8 +335,11 @@ def test_update_invalid_in_stock_product(setup_database):
     assert response.status_code == 422
 
 
-def test_update_invalid_out_of_stock_product(setup_database):
-    """Ensuring the product will not be updated with invalid data."""
+def test_update_product_with_invalid_out_of_stock_status(setup_database):
+    """
+    Checks whether updating a product with "out_of_stock" status 
+    and stock_quantity greater than 0 returns an error.
+    """
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post(f'/products', json=generated_products[0])
@@ -313,8 +353,8 @@ def test_update_invalid_out_of_stock_product(setup_database):
     assert response.status_code == 422
 
 
-def test_update_product_with_invalid_price(setup_database):
-    """Ensuring the product will not be updated with invalid data."""
+def test_update_product_with_invalid_price_value(setup_database):
+    """Checks whether updating a product with an invalid price returns an error."""
     # Cria um produto
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post(f'/products', json=generated_products[0])
@@ -328,8 +368,8 @@ def test_update_product_with_invalid_price(setup_database):
     assert response.status_code == 422
 
 
-def test_delete_product(setup_database):
-    """Ensuring the product will be deleted."""
+def test_delete_product_successfully(setup_database):
+    """Checks whether a product can be deleted correctly."""
     generated_products: List[dict] = utils.generate_valid_products(1)
     response = client.post(f'/products', json=generated_products[0])
     created_product_id = response.json()['id']
@@ -339,8 +379,8 @@ def test_delete_product(setup_database):
     assert response.status_code == 404  # NotFound esperado após a deleção
 
 
-def test_delete_a_nonexistent_product(setup_database):
-    """Ensuring NotFound exception when trying to delete a non-existing product."""
+def test_delete_nonexistent_product(setup_database):
+    """Checks if a NotFound error is returned when trying to delete a non-existent product."""
     response = client.delete('/products/999')
     assert response.status_code == 404
     assert response.json()['message'] == 'Product not found.'
